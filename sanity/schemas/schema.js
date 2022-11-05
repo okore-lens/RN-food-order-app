05cfed0c3ae9a60ecd3a1cc0c9dae754c40a0867
lens <okorelens27@gmail.com>
2022-11-05 23:46:04 +0300

    Added Sanity CMS and Home Components

---
+++ sanity/schemas/schema.js
@@ -0,0 +1,14 @@
+// First, we must import the schema creator
+import createSchema from "part:@sanity/base/schema-creator";
+
+// Then import schema types from any plugins that might expose them
+import schemaTypes from "all:part:@sanity/base/schema-type";
+
+import category from "./category";
+import restaurant from "./restaurant";
+import dish from "./dish";
+
+export default createSchema({
+  name: "default",
+  types: schemaTypes.concat([restaurant, category, dish]),
+});
